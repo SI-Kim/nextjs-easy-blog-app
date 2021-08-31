b66602650514b31796a0aac6f85d866bc2b204fb
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-08-31 16:51:13 +0900

    fdfdg

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-import Link from 'next/link';
-import Header from '../layouts/Header';
-import Footer from '../layouts/Footer';
-import Sidebar from '../layouts/Sidebar';
-import Main from '../layouts/Main';
+import Link from "next/link";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+import Sidebar from "../layouts/Sidebar";
+import Main from "../layouts/Main";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Main/>
-      <Sidebar/>
+      <Main />
+      <Sidebar />
       <Footer />
-      
     </>
-  )
+  );
 }
