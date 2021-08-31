914f4dc182bf6a85c0af6d0423c46e94aa15bde1
sung-ilkim <skim88.1942@gmail.com>
2021-08-31 23:05:08 +0900

    navigation bar test

--- dumy-datas/headerDatas.js
+++ dumy-datas/headerDatas.js
@@ -1,6 +1,1 @@
-export const headerNav = [
-    {
-        name: "메인게시판",
-        link: "/board1/MainBoard"
-    }
-]
+export 
--- layouts/Header.js
+++ layouts/Header.js
@@ -1,15 +1,35 @@
 import styles from '../styles/layouts.module.css';
 import Link from 'next/link';
-import headerNav from '../dumy-datas/headerDatas';
+import { headerNav } from '../dumy-datas/headerDatas';
 
 export default function Header() {
+
+    const headerNav = [
+        {
+            id: 1,
+            name: "메인게시판",
+            link: "/board1/MainBoard"
+        },
+        {
+            id: 2,
+            name: "더미버튼",
+            link: "/board1/MainBoard"
+        },
+    ];
+    headerNav.map(data => (
+        console.log(data)
+    ));
+    console.log(headerNav[1]);
     return (
         <header className={styles.header}>
             <div className={styles.logoZone}>
                 empty space for logo
             </div>
             <nav className={styles.navigation}>
-            <div className={styles.item}><Link href="/board1/MainBoard">MainBoard</Link></div>
+                {headerNav.map(data => (
+                    <div key={data.id} className={styles.item}><Link href={data.link}>{data.name}</Link></div>        
+                ))}
+            {/* <div className={styles.item}><Link href="/board1/MainBoard">MainBoard</Link></div> */}
             </nav>
         </header>
     );
