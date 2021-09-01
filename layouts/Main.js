92332882825c303c369160a6d02576f99bdc697a
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-01 12:37:16 +0900

    change main width

--- layouts/Main.js
+++ layouts/Main.js
@@ -1,5 +1,13 @@
 import styles from "../styles/layouts.module.css";
 
 export default function Main() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <table>
+        <tr>
+          <td>test</td>
+        </tr>
+      </table>
+    </main>
+  );
 }
