d587b3929f95e54c909831e6eb32e94630f9b389
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-01 12:55:13 +0900

    햄버거 메뉴

--- layouts/Header.js
+++ layouts/Header.js
@@ -1,36 +1,34 @@
-import styles from '../styles/layouts.module.css';
-import Link from 'next/link';
-import { headerNav } from '../dumy-datas/headerDatas';
+import styles from "../styles/layouts.module.css";
+import Link from "next/link";
+import { headerNav } from "../dumy-datas/headerDatas";
 
 export default function Header() {
-
-    const headerNav = [
-        {
-            id: 1,
-            name: "메인게시판",
-            link: "/board1/MainBoard"
-        },
-        {
-            id: 2,
-            name: "더미버튼",
-            link: "/board1/MainBoard"
-        },
-    ];
-    headerNav.map(data => (
-        console.log(data)
-    ));
-    console.log(headerNav[1]);
-    return (
-        <header className={styles.header}>
-            <div className={styles.logoZone}>
-                empty space for logo
-            </div>
-            <nav className={styles.navigation}>
-                {headerNav.map(data => (
-                    <div key={data.id} className={styles.item}><Link href={data.link}>{data.name}</Link></div>        
-                ))}
-            {/* <div className={styles.item}><Link href="/board1/MainBoard">MainBoard</Link></div> */}
-            </nav>
-        </header>
-    );
+  const headerNav = [
+    {
+      id: 1,
+      name: "메인게시판",
+      link: "/board1/MainBoard"
+    },
+    {
+      id: 2,
+      name: "더미버튼",
+      link: "/board1/MainBoard"
+    }
+  ];
+  headerNav.map((data) => console.log(data));
+  console.log(headerNav[1]);
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoZone}>empty space for logo</div>
+      <nav className={styles.navigation}>
+        <div className={styles.hamberger}>三</div>
+        {headerNav.map((data) => (
+          <div key={data.id} className={styles.item}>
+            <Link href={data.link}>{data.name}</Link>
+          </div>
+        ))}
+        {/* <div className={styles.item}><Link href="/board1/MainBoard">MainBoard</Link></div> */}
+      </nav>
+    </header>
+  );
 }
