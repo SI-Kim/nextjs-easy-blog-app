e5539dd63cb9e2a153d5775eea392a23bdb799a4
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-02 01:44:22 +0900

    test

--- layouts/Header.js
+++ layouts/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
       <nav className={styles.navigation}>
         <img
           className={styles.hamberger}
-          onClick={handleHambergerMenu()}
+          onClick={() => handleHambergerMenu()}
           src={hambergerUrl}
           alt="hamberger"
         />
