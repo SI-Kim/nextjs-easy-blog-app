12c4a072bdcb09f4d602593af02044fdf7468264
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-03 16:40:19 +0900

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import Sidebar from "../layouts/Sidebar";
-import Main from "../layouts/Main";
-import '../styles/globals.css'
+import styles from "../styles/layouts.module.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <Component {...pageProps} className={styles.main} />
       <Sidebar />
       <Footer />
     </>
   );
-  
 }
 
-export default MyApp
+export default MyApp;
