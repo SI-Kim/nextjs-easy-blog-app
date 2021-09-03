49ace4cd8dc465567becf93c93cf053d4bb5b940
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-04 00:46:47 +0900

    ㄴ

--- layouts/Footer.js
+++ layouts/Footer.js
@@ -1,5 +1,5 @@
 import styles from "../styles/layouts.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return <footer className={styles.footer}>이 영역은 Footer 입니다.</footer>;
 }
