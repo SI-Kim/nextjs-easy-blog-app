cbda33987743347af4408c536c260039c1283922
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-05 20:14:22 +0900

    테이블생성

--- pages/board1/MainBoard.js
+++ pages/board1/MainBoard.js
@@ -1,5 +1,51 @@
 import styles from "../../styles/layouts.module.css";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
 
 export default function MainBoard() {
-  return <div className={styles.main}>이 곳은 메인 게시판 입니다.</div>;
+  const mainBoardDatas = [
+    {
+      id: 1,
+      date: "2021-01-01",
+      title: "더미 데이터 제목 1",
+      content: "터미데이터 내용 1",
+      author: "더미데이터",
+      link: "/board1/MainBoard"
+    },
+    {
+      id: 2,
+      date: "2021-03-03",
+      title: "더미데이터 제목 2asdfojhjasdlkfsad",
+      content: "더미데이터 내용 2 sdafjlkasjdlfksdfa",
+      author: "asdfkljasdllfksjd",
+      link: "/board1/MainBoard"
+    }
+  ];
+  return (
+    <div className={styles.main}>
+      <Table>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>제목</TableCell>
+          <TableCell>날짜</TableCell>
+          <TableCell>작성자</TableCell>
+        </TableHead>
+        <TableBody>
+          {mainBoardDatas.map((data) => (
+            <TableRow key={data.id}>
+              <TableCell>{data.id}</TableCell>
+              <TableCell>{data.title}</TableCell>
+              <TableCell>{data.date}</TableCell>
+              <TableCell>{data.author}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
