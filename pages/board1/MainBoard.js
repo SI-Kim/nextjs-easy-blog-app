767c8aa4ee99db3fab1fe666f915dcf851e103f4
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-05 20:37:04 +0900

    table 수정

--- pages/board1/MainBoard.js
+++ pages/board1/MainBoard.js
@@ -1,11 +1,14 @@
 import styles from "../../styles/layouts.module.css";
 import {
+  TableContainer,
   Table,
   TableHead,
   TableBody,
   TableRow,
   TableCell
 } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
 export default function MainBoard() {
   const mainBoardDatas = [
@@ -26,28 +29,49 @@ export default function MainBoard() {
       link: "/board1/MainBoard"
     }
   ];
+
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white
+    },
+    body: {
+      fontSize: 14
+    }
+  }))(TableCell);
+
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover
+      }
+    }
+  }))(TableRow);
+
   return (
     <div className={styles.main}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>제목</TableCell>
-            <TableCell>날짜</TableCell>
-            <TableCell>작성자</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {mainBoardDatas.map((data) => (
-            <TableRow key={data.id}>
-              <TableCell>{data.id}</TableCell>
-              <TableCell>{data.title}</TableCell>
-              <TableCell>{data.date}</TableCell>
-              <TableCell>{data.author}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <TableContainer component={Paper} style={{ margin: "10px" }}>
+        <Table>
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell>번호</StyledTableCell>
+              <StyledTableCell>제목</StyledTableCell>
+              <StyledTableCell>날짜</StyledTableCell>
+              <StyledTableCell>작성자</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {mainBoardDatas.map((data) => (
+              <StyledTableRow key={data.id}>
+                <StyledTableCell>{data.id}</StyledTableCell>
+                <StyledTableCell>{data.title}</StyledTableCell>
+                <StyledTableCell>{data.date}</StyledTableCell>
+                <StyledTableCell>{data.author}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
