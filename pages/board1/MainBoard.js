46d7fbebf50ac8b910e55ab9858563b5df547314
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-09-07 17:22:34 +0900

    main board 수정

--- pages/board1/MainBoard.js
+++ pages/board1/MainBoard.js
@@ -8,7 +8,15 @@ import {
   TableCell
 } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
 
 export default function MainBoard() {
   const mainBoardDatas = [
@@ -50,28 +58,20 @@ export default function MainBoard() {
 
   return (
     <div className={styles.main}>
-      <TableContainer component={Paper} style={{ margin: "10px" }}>
-        <Table>
-          <TableHead>
-            <StyledTableRow>
-              <StyledTableCell>번호</StyledTableCell>
-              <StyledTableCell>제목</StyledTableCell>
-              <StyledTableCell>날짜</StyledTableCell>
-              <StyledTableCell>작성자</StyledTableCell>
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>
-            {mainBoardDatas.map((data) => (
-              <StyledTableRow key={data.id}>
-                <StyledTableCell>{data.id}</StyledTableCell>
-                <StyledTableCell>{data.title}</StyledTableCell>
-                <StyledTableCell>{data.date}</StyledTableCell>
-                <StyledTableCell>{data.author}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Card style={{ maxWidth: "240px", margin: "20px" }}>
+        <CardHeader
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        ></CardHeader>
+        <CardMedia image="/logo2.png" title="Paella dish" />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
